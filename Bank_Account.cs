0e25f46038b776ec84ee339011e660dfe010456a
Moonyll <s.pouwel@laposte.net>
2019-01-31 09:38:52 +0100

    PO7

---
+++ Bank_Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace PO7
+//{
+//    class Bank_Account
+//    {
+//        public string _owner;
+//        public string _currency;
+//        public double _final_settlement;
+//        public void _tocredit(double _final_settlement)
+//        {
+//        _final_settlement = _final_settlement+250;
+//        }
+//        public void _todebit(double _final_settlement)
+//        {
+//        _final_settlement = _final_settlement-250;
+//        }
+//    }
+//}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Bank_Account   // Création de la class Bank_Account
+    {
+        //déclaration des attributs
+        public string owner;    // Titulaire
+        public string currency; // Devise
+        public double final_settlement;                 // Solde tout compte
+        // Méthode pour le crédit. Une méthode qui a comme paramètre amount qui retourne du vide
+        public void Tocredit(double amount)  
+        {
+            final_settlement = final_settlement + amount;  // Crédit de amount
+        }
+        // Méthode pour le débit. Une méthode qui a comme paramètre amount qui retourne du vide
+        public void Todebit(double amount)
+        {
+            final_settlement = final_settlement - amount; // Débit de amount
+        }
+    }
+ }
+
